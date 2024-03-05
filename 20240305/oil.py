b7ef5a5ca54531a796099d7d1cd5cbf156798e0f
Cheng-Tao Yang <40634259+chengtaoyang@users.noreply.github.com>
2024-03-05 19:53:50 +0800

    Update oil.py

--- 20240305/oil.py
+++ 20240305/oil.py
@@ -2,24 +2,27 @@ import requests
 from bs4 import BeautifulSoup
 import pandas as pd
 from matplotlib.font_manager import FontProperties
-from matplotlib.font_manager import FontProperties
 
-# 獲取網頁內容
+#https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil2019.aspx
+# 獲取 2019年前的網頁內容
+response = requests.get('https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil2019.aspx')
+soup = BeautifulSoup(response.text, 'html.parser')
+tables = soup.find_all('table')
+df3 = pd.read_html(str(tables[1]))[0]
+
+print(df3)
+
+# 獲取網頁 2000 後的內容
 response = requests.get('https://vipmbr.cpc.com.tw/mbwebs/ShowHistoryPrice_oil.aspx')
-
 # 解析網頁內容
 soup = BeautifulSoup(response.text, 'html.parser')
-
 # 找到所有的表格元素
 tables = soup.find_all('table')
-
 # 將 HTML 表格轉換為 DataFrame
-df1 = pd.read_html(str(tables[0]))[0]
 df2 = pd.read_html(str(tables[1]))[0]
 
-# 印出 DataFrame
-print(df1)
-print(df2)
+#將df3 與 df2 合併 變成 df2 
+df2 = pd.concat([df3, df2])
 
 # 將 DataFrame 寫入 CSV 檔案
 df2.to_csv("C:/Users/USER/Desktop/oil.csv", index=False)
@@ -30,8 +33,10 @@ df2 = df2.iloc[:, :5]
 # 去除值是NaN的資料
 df2 = df2.dropna()
 
+print (df2)
+
 # 把第一欄的資料型態 轉成 datetime
-df2[df2.columns[0]] = pd.to_datetime(df2[df2.columns[0]])
+#df2[df2.columns[0]] = pd.to_datetime(df2[df2.columns[0]])
 
 print (df2)
 
@@ -51,3 +56,5 @@ plt.plot(df2[df2.columns[0]], df2[df2.columns[4]], label='超級柴油')
 plt.legend()
 # 顯示圖片
 plt.show()
+plt.savefig("C:/Users/USER/Desktop/oil.png")
+print
