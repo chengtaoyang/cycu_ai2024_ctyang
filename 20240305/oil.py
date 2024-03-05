0e0d4757e5e3cd1584e6aa850281d5f80dd1d9d3
Cheng-Tao Yang <40634259+chengtaoyang@users.noreply.github.com>
2024-03-05 20:22:58 +0800

    update

--- 20240305/oil.py
+++ 20240305/oil.py
@@ -33,12 +33,10 @@ df2 = df2.iloc[:, :5]
 # 去除值是NaN的資料
 df2 = df2.dropna()
 
-print (df2)
+#使用第一欄的資料 重新排序
+df2 = df2.sort_values(by=df2.columns[0])
 
-# 把第一欄的資料型態 轉成 datetime
-#df2[df2.columns[0]] = pd.to_datetime(df2[df2.columns[0]])
 
-print (df2)
 
 # 使用 matplotlib x,y 折線圖 , x 軸是日期 , 後面四個欄位是 油價 ，分別是 92無鉛汽油,95無鉛汽油,98無鉛汽油,超級柴油 
 import matplotlib.pyplot as plt
@@ -46,7 +44,7 @@ import matplotlib.pyplot as plt
 plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei']
 plt.rcParams['axes.unicode_minus'] = False
 # 設定圖片大小
-plt.figure(figsize=(10, 6))
+plt.figure(figsize=(60, 6))
 # 繪製折線圖
 plt.plot(df2[df2.columns[0]], df2[df2.columns[1]], label='92無鉛汽油')
 plt.plot(df2[df2.columns[0]], df2[df2.columns[2]], label='95無鉛汽油')
@@ -54,6 +52,10 @@ plt.plot(df2[df2.columns[0]], df2[df2.columns[3]], label='98無鉛汽油')
 plt.plot(df2[df2.columns[0]], df2[df2.columns[4]], label='超級柴油')
 # 設定圖例
 plt.legend()
+#設定 x軸輸出的格式 , x軸的標籤是日期 每隔 30天顯示一次
+plt.xticks(rotation=45)
+plt.xticks(df2[df2.columns[0]][::30])
+# 設定圖表標題
 # 顯示圖片
 plt.show()
 plt.savefig("C:/Users/USER/Desktop/oil.png")
