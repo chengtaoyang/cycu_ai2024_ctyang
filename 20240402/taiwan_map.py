08c920ae0f40a0853925aec358c0991983f167e4
ubuntu@hermes.hodala.tw <ctyang@hodala.tw>
2024-04-02 09:55:01 +0000

    homework

--- 20240402/taiwan_map.py
+++ 20240402/taiwan_map.py
@@ -1,2 +1,12 @@
 #尋找台灣的區界地圖 shape file/ geojson file
 #https://data.gov.tw/dataset/7441
+
+#read 20240402/tract_20140313.json as geopanas
+import geopandas as gpd
+import pandas as pd
+
+#read geojson file
+taiwan=gpd.read_file('20240402/tract_20140313.json')
+import os
+#print cwd
+print (taiwan.shape)
