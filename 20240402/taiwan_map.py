3cdb9b5c0c5c1d3ee5a11b1c69d84741cdc845d2
Cheng-Tao Yang <40634259+chengtaoyang@users.noreply.github.com>
2024-04-02 18:23:53 +0800

    homework

--- 20240402/taiwan_map.py
+++ 20240402/taiwan_map.py
@@ -5,8 +5,15 @@
 import geopandas as gpd
 import pandas as pd
 
-#read geojson file
-taiwan=gpd.read_file('20240402/tract_20140313.json')
+#read shape file
+taiwan=gpd.read_file('20240402/county/county_moi_1090820.shp')
 import os
 #print cwd
 print (taiwan.shape)
+
+#plot taiwan using matplotlib
+import matplotlib.pyplot as plt
+taiwan.plot()
+plt.show()
+#save to png file
+plt.savefig('20240402/taiwan_map.png')
